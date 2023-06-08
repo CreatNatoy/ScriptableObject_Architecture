f6d672f6b920f66596c75b8cd27d85c24721b328
Anton Pashkevich <76531899+CreatNatoy@users.noreply.github.com>
2023-06-08 10:31:19 +0200

    Add odin Assets

---
+++ Assets/Configuration/Scripts/ParametersSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Configuration.Scripts
+{
+    [CreateAssetMenu(fileName = "ParametersSo", menuName = "ScriptableObject/ParametersSo")]
+    public class ParametersSo : ScriptableObject
+    {
+        [SerializeField] private int _maxHealth;
+        [SerializeField] private int _speed;
+        [SerializeField] private int _damage;
+        [SerializeField] private int _defence;
+    }
+}
